dc648d36a4d5632e2bd6c3076a918ccca27bbcf2
marine <kasskaka1212@gmail.com>
2024-09-18 22:10:32 +0900

    java_basic/24.09.18/2

--- src/main/java/MiniProject/PostRepository.java
+++ src/main/java/MiniProject/PostRepository.java
@@ -4,4 +4,10 @@ import java.util.ArrayList;
 
 public class PostRepository {
    private ArrayList<Post> PostsList = new ArrayList<>();
+
+   public void save (Post post){
+      PostsList.add(post);
+   }
+
+
 }
